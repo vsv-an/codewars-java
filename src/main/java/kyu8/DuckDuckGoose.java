90b32d4f0e856ff732d8b5bac9bdd104d6988d92
ViacheslavAnisimov <forvsn@gmail.com>
2023-02-06 07:54:51 +0700

    add new kata

---
+++ src/main/java/kyu8/DuckDuckGoose.java
@@ -0,0 +1,9 @@
+package kyu8;
+
+public class DuckDuckGoose {
+//    public static String duckDuckGoose(Player[] players, int goose) {
+//        // Note: Player objects have a String field called 'name'.
+//
+//        return players[(goose - 1) % players.length)].name;
+//    }
+}
