a634a00b57387e99b1055ea55b2b194745138225
ViacheslavAnisimov <forvsn@gmail.com>
2023-03-09 13:43:28 +0700

    added new kata

--- README.md
+++ README.md
@@ -170,3 +170,4 @@
 | CamelCase Method        | [Link](https://www.codewars.com/kata/587731fda577b3d1b0001196) | [Link](./src/main/java/kyu6/CamelCaseMethod.java)       |
 | CamelCase to underscore | [Link](https://www.codewars.com/kata/5b1956a7803388baae00003a) | [Link](./src/main/java/kyu6/CamelCaseToUnderscore.java) |
 | Multiplication table    | [Link](https://www.codewars.com/kata/534d2f5b5371ecf8d2000a08) | [Link](./src/main/java/kyu6/MultiplicationTable.java)   |
+| Who likes it?    | [Link](https://www.codewars.com/kata/5266876b8f4bf2da9b000362) | [Link](./src/main/java/kyu6/WhoLikesIt.java)   |
---
+++ src/test/java/kyu6/WhoLikesItTest.java
@@ -0,0 +1,17 @@
+package kyu6;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class WhoLikesItTest {
+    WhoLikesIt Solution = new WhoLikesIt();
+    @Test
+    public void staticTests() {
+        assertEquals("no one likes this", Solution.whoLikesIt());
+        assertEquals("Peter likes this", Solution.whoLikesIt("Peter"));
+        assertEquals("Jacob and Alex like this", Solution.whoLikesIt("Jacob", "Alex"));
+        assertEquals("Max, John and Mark like this", Solution.whoLikesIt("Max", "John", "Mark"));
+        assertEquals("Alex, Jacob and 2 others like this", Solution.whoLikesIt("Alex", "Jacob", "Mark", "Max"));
+    }
+}
