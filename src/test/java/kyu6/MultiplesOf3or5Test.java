cbe437abada131b48025bcbaf10553945b8a343a
ViacheslavAnisimov <forvsn@gmail.com>
2023-03-14 11:14:36 +0700

    added new kata

---
+++ src/test/java/kyu6/MultiplesOf3or5Test.java
@@ -0,0 +1,11 @@
+package kyu6;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MultiplesOf3or5Test {
+    @Test
+    public void test() {
+        assertEquals(23, new MultiplesOf3or5().solution(10));
+    }
+}
