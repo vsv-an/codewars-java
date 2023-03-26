4a5657dba8369955391a593dccd2bb1793e814b5
ViacheslavAnisimov <forvsn@gmail.com>
2023-03-26 11:34:32 +0700

    edit kata

--- src/test/java/kyu8/QuarterOfTheYearTest.java
+++ src/test/java/kyu8/QuarterOfTheYearTest.java
@@ -2,13 +2,21 @@ package kyu8;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+
 // TODO: Replace examples and use TDD by writing your own tests
 
 public class QuarterOfTheYearTest {
+
     @Test
     public void exampleTests() {
 //        Tester.doTest(3, 1);
 //        Tester.doTest( 8, 3);
 //        Tester.doTest(11, 4);
+        assertEquals(1, new QuarterOfTheYear().quarterOf(3));
+        assertEquals(3, new QuarterOfTheYear().quarterOf(8));
+        assertEquals(4, new QuarterOfTheYear().quarterOf(11));
     }
 }
